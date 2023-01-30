9978a8432b57e71fe69a5ea4e96fc961dcb33618
steve2700 <stevnyaruwa@gmail.com>
2023-01-30 10:23:21 +0200

    best school

---
+++ for_bestloops.sh
@@ -0,0 +1,11 @@
+#Write a Bash script that displays Best School 10 times.
+
+#Requirement:
+
+#You must use the for loop (while and until are forbidden)
+
+for i in {1..10}
+do
+	echo "Best School"
+done
+																					
